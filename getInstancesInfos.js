974ae5db3b634ce00f89bac11f0cdef5ed98dd8d
tamaina <tamaina@hotmail.co.jp>
2022-09-10 22:33:28 +0900

    改行

--- getInstancesInfos.js
+++ getInstancesInfos.js
@@ -76,7 +76,14 @@ async function postJson(url, json) {
 }
 
 // syuilo/misskeyを最後に持っていくべし
-const ghRepos = ["mei23/misskey", "mei23/misskey-v11", "TeamBlackCrystal/misskey", "Groundpolis/Groundpolis", "kokonect-link/cherrypick", "syuilo/misskey"];
+const ghRepos = [
+	"mei23/misskey",
+	"mei23/misskey-v11",
+	"TeamBlackCrystal/misskey",
+	"Groundpolis/Groundpolis",
+	"kokonect-link/cherrypick",
+	"syuilo/misskey"
+];
 
 module.exports.ghRepos = ghRepos;
 
